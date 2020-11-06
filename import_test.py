f680f1de2b35ddf5ca35d48c69e9445a0cbb0566
Aparna <aparna.radhakrishnan@noaa.gov>
2020-11-06 10:42:44 -0500

    move back for now

---
+++ import_test.py
@@ -0,0 +1,13 @@
+try:
+  import intakebuilder
+  print("Imported intakebuilder")
+  try:
+     from intakebuilder import getinfo, localcrawler, CSVwriter
+     print("Imported getinfo, localcrawler, CSVwriter")
+     return -99
+  except ImportError:
+     print("Unable to import",  "getinfo, localcrawler, CSVwriter")
+     return -98
+except ImportError:
+  print("Unable to import intakebuilder")
+  return -97
