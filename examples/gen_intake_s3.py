c0d92699372d8574406f7daa41fd6b0604daadb9
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2020-11-09 17:36:10 -0500

    Update gen_intake_s3.py
    lost in translation, old version

--- examples/gen_intake_s3.py
+++ examples/gen_intake_s3.py
@@ -1,33 +1,31 @@
-#!/usr/bin/env python
 import os
-from intakebuilder import getinfo, localcrawler, CSVwriter
+from intakebuilder import getinfo, s3crawler, CSVwriter
 
-import logging
-logger = logging.getLogger('local')
-hdlr = logging.FileHandler('/Users/ar46/PycharmProjects/CatalogBuilder/logs/local.log')
-logger.addHandler(hdlr)
-logger.setLevel(logging.INFO)
 
 def main():
     #######INPUT HERE OR USE FROM A CONFIG FILE LATER######
-    project_dir = "/Users/ar46/data_cmip6/CMIP6/"  # DRS COMPLIANT PROJECT DIR
-    csvfile = "/Users/ar46/PycharmProjects/CatalogBuilder/intakebuilder/test/intake_local.csv"
-    #######################################################
+    region = 'us-east-1' #which region is the bucket in?
+    project_root = 's3://gfdl-esgf/CMIP6/' #DRS Compliant bucket
+    csvfile = "/Users/ar46/PycharmProjects/CatalogBuilder/intakebuilder/test/intake_s3.csv"
     ######### SEARCH FILTERS ###########################
-    dictFilter = {} #CMIP6 CMIP6/CMIP/NOAA-GFDL/GFDL-ESM4/
-    dictFilter["source_prefix"] = 'CMIP6/AerChemMIP/NOAA-GFDL/GFDL-ESM4'  # /CMIP/NOAA-GFDL/GFDL-ESM4/' #Must specify something here, at least the project level
-    # COMMENT  dictFilter["miptable"] = "Amon" #Remove this if you don't want to filter by miptable
-    # COMMENT dictFilter["varname"] = "tas"   #Remove this if you don't want to filter by variable name
+    dictFilter = {}
+    dictFilter["source_prefix"]= 'CMIP6/' #/CMIP/NOAA-GFDL/GFDL-ESM4/' #Must specify something here, at least the project level
+   #COMMENT  dictFilter["miptable"] = "Amon" #Remove this if you don't want to filter by miptable
+   #COMMENT dictFilter["varname"] = "tas"   #Remove this if you don't want to filter by variable name
     #######################################################
+    project_bucket = project_root.split("/")[1].lstrip("/")
+    project_name = project_root.split("/")[2]
     dictInfo = {}
-    project_dir = project_dir.rstrip("/")
-    print("Running")
-    list_files = localcrawler.crawlLocal(project_dir, dictFilter,logger)
+    print(project_root)
+    project_root = project_root.rstrip("/")
+    list_files = s3crawler.sss_crawler(project_root,dictFilter, project_root,dictInfo={})
+    print(list_files)
+    #TODO make search strings a dict for later
+    #merge project_root and project_bucket as needed
     headers = CSVwriter.getHeader()
     if (not os.path.exists(csvfile)):
         os.makedirs(os.path.dirname(csvfile), exist_ok=True)
     CSVwriter.listdict_to_csv(list_files, headers, csvfile)
-    logger.info("CSV generated at"+ os.path.abspath(csvfile))
     print("CSV generated at:", os.path.abspath(csvfile))
 
 
