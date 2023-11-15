0c43021dff2743e08148fdb2b7f8639845107d2f
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2023-11-15 10:49:23 -0500

    Update subdirs.py

--- tests/subdirs.py
+++ tests/subdirs.py
@@ -1,5 +1,7 @@
-#utils for tests/make_sample_data.py
-#the sub-directories and other patterns in the directory strucure and file names needed for testing are defined below, assuming there is a root defined in the caller script
+""" 
+utils for tests/make_sample_data.py
+the sub-directories and other patterns in the directory strucure and file names needed for testing are defined below, assuming there is a root defined in the caller script
+"""
 
 realm = [
 'atmos',
