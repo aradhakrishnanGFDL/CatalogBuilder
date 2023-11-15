e2dbd24dcc99d01b93ac2261e58901cd33fe601d
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2023-11-15 10:47:21 -0500

    Update make_sample_data.py
    summary added

--- tests/make_sample_data.py
+++ tests/make_sample_data.py
@@ -1,3 +1,16 @@
+"""
+This script generates a sample directory structure with a few PP components and empty netcdf files. 
+There is a subdirs.py in the tests directory that provides info as to how the structure can be, while the root structure is defined in the script below.
+In the near future, this will allow us to add more patterns and directory structure expectations, components,different chunks, time series/time average etc and expand
+the following script and the subdirs.py. 
+
+Context for this script- We use this in our GitHub Actions workflow to create sample directories on the fly before running the GFDL catalog builder 
+script dynamically.
+
+Author: A.Radhakrishnan and workflow team
+Date: Nov 15, 2023
+
+"""
 import os
 import subdirs
 from subdirs import *
