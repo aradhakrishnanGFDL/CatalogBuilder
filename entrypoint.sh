22f1fedafea7232556304bff9f07c2e30eb4e499
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2023-11-15 09:27:11 -0500

    Delete entrypoint.sh
    Delete unused file

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash --login
-# The --login ensures the bash configuration is loaded,
-# enabling Conda.
-
-# Temporarily disable strict mode and activate conda:
-conda activate catalogbuilder 
-
