214bd429997afe733ef37f88651109091380ff9c
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-01-19 16:05:05 -0500

    Update setup.py
    rn requirements reference

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from distutils.core import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
-    requirements = f.read().strip().split('\n')
-
 setup(
 name='intakebuilder',
 packages=['intakebuilder',],
