86e1450025de8747f571c86064d852b1bd5e02a5
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-01-19 12:28:00 -0500

    conda packaging progress

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from distutils.core import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    requirements = f.read().strip().split('\n')
+
 setup(
 name='intakebuilder',
-version='0.1dev',
 packages=['intakebuilder',],
+description='intake-esm Catalog Generation Utilities',
 license='Creative Commons Attribution-Noncommercial-Share Alike license'
 )
