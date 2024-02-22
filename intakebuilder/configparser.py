81b07d75cbbc50c7567d392f82e8177ee8e6fecd
AR from AOS <aparna.radhakrishnan@noaa.gov>
2024-02-22 15:50:20 -0500

    configparser to create Config class

--- intakebuilder/configparser.py
+++ intakebuilder/configparser.py
@@ -1,13 +1,17 @@
 import yaml
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
-try:
-    input_path = config['input_path']
-    print(input_path)
-except:
-    raise KeyError("input_path does not exist in config")
-try:
-    output_path = config['output_path']
-    print(output_path)
-except:
-    raise KeyError("output_path does not exist in config")
+
+class Config:
+   def __init__(self,config):
+     self.config = config 
+     with open(self.config, 'r') as file:
+        configfile = yaml.safe_load(file)
+     try:
+         self.input_path = configfile['input_path']
+         print(self.input_path)
+     except:
+         raise KeyError("input_path does not exist in config")
+     try:
+         self.output_path = configfile['output_path']
+         print(self.output_path)
+     except:
+         raise KeyError("output_path does not exist in config")
