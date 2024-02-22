01d4ee2367c18e227efcd3c4d19b18aec877319a
AR from AOS <aparna.radhakrishnan@noaa.gov>
2024-02-22 17:19:01 -0500

    cl args

---
+++ intakebuilder/examples/gen_intake_gfdl_runner.py
@@ -0,0 +1,10 @@
+import gen_intake_gfdl
+import sys
+
+
+input_path = "/Users/ar46/archive/am5/am5/am5f3b1r0/c96L65_am5f3b1r0_pdclim1850F/gfdl.ncrc5-deploy-prod-openmp/pp/"
+output_path = "test"
+sys.argv = ['INPUT_PATH', input_path, output_path]
+print(sys.argv)
+gen_intake_gfdl.main()
+
