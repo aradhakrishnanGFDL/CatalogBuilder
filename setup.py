b24c85d3dcb5d85ddebf6af7d59378c30e30448a
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-02-28 10:09:39 -0500

    Update setup.py
    bugfix https://github.com/aradhakrishnanGFDL/CatalogBuilder/issues/62

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
 name='intakebuilder',
-packages=['intakebuilder',],
+version='0.1.3',
+packages=find_packages(),
+include_package_data=True,
 description='intake-esm Catalog Generation Utilities',
 license='Creative Commons Attribution-Noncommercial-Share Alike license'
 )
+
+
