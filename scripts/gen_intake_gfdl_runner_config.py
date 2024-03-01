085ba1f3bb82751875e112dc01c0b01fef21da85
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-03-01 12:38:48 -0500

    from scripts

--- scripts/gen_intake_gfdl_runner_config.py
+++ scripts/gen_intake_gfdl_runner_config.py
@@ -3,7 +3,7 @@
 from scripts import gen_intake_gfdl
 import sys
 
-sys.argv = ['--config', '/home/a1r/github/CatalogBuilder/configs/config.yaml']
+sys.argv = ['input_path','--config', '/home/a1r/github/CatalogBuilder/scripts/configs/config-example.yml']
 print(sys.argv)
 gen_intake_gfdl.main()
 
