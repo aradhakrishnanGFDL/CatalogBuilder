7a16f7532b2251d512717cd087d75eb2ed5bd4d0
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-03-01 11:41:29 -0500

    config template added

--- scripts/gen_intake_gfdl_runner_config.py
+++ scripts/gen_intake_gfdl_runner_config.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-import gen_intake_gfdl
+from scripts import gen_intake_gfdl
 import sys
 
-sys.argv = ['INPUT_PATH',None,'--config', '/home/a1r/github/CatalogBuilder/configs/config.yaml']
+sys.argv = ['--config', '/home/a1r/github/CatalogBuilder/configs/config.yaml']
 print(sys.argv)
 gen_intake_gfdl.main()
 
