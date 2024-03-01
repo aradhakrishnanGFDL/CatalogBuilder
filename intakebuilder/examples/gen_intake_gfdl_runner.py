82e2ec8c76110d0e6702e1deb8010b7224219e80
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-03-01 09:03:51 -0500

    runner updated

--- intakebuilder/examples/gen_intake_gfdl_runner.py
+++ intakebuilder/examples/gen_intake_gfdl_runner.py
@@ -2,7 +2,7 @@ import gen_intake_gfdl
 import sys
 
 
-input_path = "/Users/ar46/archive/am5/am5/am5f3b1r0/c96L65_am5f3b1r0_pdclim1850F/gfdl.ncrc5-deploy-prod-openmp/pp/"
+input_path = "/archive/am5/am5/am5f3b1r0/c96L65_am5f3b1r0_pdclim1850F/gfdl.ncrc5-deploy-prod-openmp/pp/"
 output_path = "test"
 sys.argv = ['INPUT_PATH', input_path, output_path]
 print(sys.argv)
--- intakebuilder/examples/gen_intake_gfdl_runner_config.py
+++ intakebuilder/examples/gen_intake_gfdl_runner_config.py
@@ -2,7 +2,7 @@ import gen_intake_gfdl
 import sys
 
 
-sys.argv = ['--config', '/Users/ar46/git/CatalogBuilder/config.yaml']
+sys.argv = ['--config', '/home/a1r/github/CatalogBuilder/configs/config.yaml']
 print(sys.argv)
 gen_intake_gfdl.main()
 
