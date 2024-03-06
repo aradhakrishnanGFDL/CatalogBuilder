8182e55e865607947a3bb5231f654ba8ec458f77
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-03-06 11:27:16 -0500

    Update gen_intake_gfdl_runner.py

--- scripts/gen_intake_gfdl_runner.py
+++ scripts/gen_intake_gfdl_runner.py
@@ -7,5 +7,5 @@ input_path = "/archive/am5/am5/am5f3b1r0/c96L65_am5f3b1r0_pdclim1850F/gfdl.ncrc5
 output_path = "test"
 sys.argv = ['INPUT_PATH', input_path, output_path]
 print(sys.argv)
-gen_intake_gfdl.main()
+scripts.gen_intake_gfdl.main()
 
