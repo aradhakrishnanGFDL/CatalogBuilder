63ff1d981809a8011f6191345f1411f0745a8a5b
Ciheim Brown <Ciheim.Brown@ldt-4788508.gfdl.noaa.gov>
2024-07-10 13:38:32 -0400

    Changing Path

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -21,7 +21,7 @@ def load_cat(catspec=None):
   return cat
 def test_loadcat():
   #TODO generate csv on the fly, check if its readable etc
-  catspec = pathlib.Path(os.path.dirname(__file__)).parent / 'cats/gfdl_test1.json'
+  catspec = pathlib.Path(os.path.dirname(__file__)).parent / 'cats/gfdl_template.json'
   #TODO generate test catalog on the fly, push spec to the test directory
   cat = load_cat((str(catspec)))
   try:
